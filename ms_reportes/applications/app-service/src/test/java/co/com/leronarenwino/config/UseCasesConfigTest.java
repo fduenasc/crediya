14ed766cf51bb9e1a4740e73b264e1217efd09af
Francisco Dueñas <Sebasduca@gmail.com>
2025-09-22 12:57:36 -0500

    feat(ms_reportes): Add ClientValidatorService mock to UseCasesConfigTest for improved test coverage

--- ms_reportes/applications/app-service/src/test/java/co/com/leronarenwino/config/UseCasesConfigTest.java
+++ ms_reportes/applications/app-service/src/test/java/co/com/leronarenwino/config/UseCasesConfigTest.java
@@ -1,5 +1,6 @@
 package co.com.leronarenwino.config;
 
+import co.com.leronarenwino.model.gateway.ClientValidatorService;
 import co.com.leronarenwino.model.gateway.ReportGateway;
 import co.com.leronarenwino.usecase.GetTotalApprovedLoansUseCase;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,12 @@ class UseCasesConfigTest {
 
     @Test
     void saveLoanApplicationUseCaseBeanIsCreated() {
+        ClientValidatorService clientValidatorService = Mockito.mock(ClientValidatorService.class);
         ReportGateway reportGateway = Mockito.mock(ReportGateway.class);
 
         UseCasesConfig config = new UseCasesConfig();
         GetTotalApprovedLoansUseCase useCase = config.getTotalApprovedLoansUseCase(
+                clientValidatorService,
                 reportGateway
         );
 
